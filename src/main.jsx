9244340b1c502f2250ba7110fa8e820c2f128cfe
jricciardulli-afk <jricciardulli@gmail.com>
2026-05-04 17:20:40 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-          <meta charset="UTF-8" />
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App />
-        </React.StrictMode>,
-        )<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>ParrillaStock</title>
-    </head>
-  <body>
-        <div id="root"></div>div>
-        <script type="module" src="/src/main.jsx"></script>
-  </body>
-</html>html>
-  </body></title>
-    </head>
+    <App />
+  </React.StrictMode>,
+)
